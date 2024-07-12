9711c06e69daf58e0f0dec445e6e930865ac102a
Svetlana <bel-svetik@yandex.ru>
2024-07-12 15:49:30 +0300

    Дополнительное задание. Добавлено решение с процессами v3 (рабочая).

--- lesson/03_volatility_with_processes.py
+++ lesson/03_volatility_with_processes.py
@@ -24,28 +24,67 @@
 from os import walk, path
 import csv
 import logging
-from multiprocessing import Process
+import multiprocessing as mp
 from time import time
 
 
 # from pprint import pprint
 
-class TickerReader(Process):
+def print_rez(data):
+    """
+    Печать результата в формате:
+        Максимальная волатильность:
+        ТИКЕР1 - ХХХ.ХХ %
+        ТИКЕР2 - ХХХ.ХХ %
+        ТИКЕР3 - ХХХ.ХХ %
+    Минимальная волатильность:
+        ТИКЕР4 - ХХХ.ХХ %
+        ТИКЕР5 - ХХХ.ХХ %
+        ТИКЕР6 - ХХХ.ХХ %
+    Нулевая волатильность:
+        ТИКЕР7, ТИКЕР8, ТИКЕР9, ТИКЕР10, ТИКЕР11, ТИКЕР12
+    Волатильности указывать в порядке убывания. Тикеры с нулевой волатильностью упорядочить по имени.
+    :param data: Словарь с данными для вывода
+    :return: None
+    """
+    log = logging.getLogger(__name__)
+    # ticker_list = list(map(lambda x, y: [y, x], data.keys(), data.values()))
+    ticker_list = list(zip(data.values(), data.keys()))
+    ticker_list.sort()
+    zero_list = []
+    while ticker_list and ticker_list[0][0] == 0:
+        zero_list.append(ticker_list[0][1])
+        ticker_list.pop(0)
+    zero_list.sort()
+    try:
+        print('\nМаксимальная волатильность:')
+        for i in range(3):
+            print(
+                f'\t{ticker_list[len(ticker_list) - i - 1][1]} - {ticker_list[len(ticker_list) - i - 1][0]:0.2f} %')
+        print('\nМинимальная волатильность:')
+        for i in range(3):
+            print(f'\t{ticker_list[2 - i][1]} - {ticker_list[2 - i][0]:0.2f} %')
+    except Exception as e:
+        log.error(e.args)
+    print('\nНулевая волатильность:')
+    print(', '.join(zero_list))
 
-    def __init__(self, file_path):
-        super().__init__()
-        self.file_path = file_path
 
-    def run(self):
-        # log = logging.getLogger(__name__)
-        with open(self.file_path, newline='') as cf:
+class TickerReader:
+    TickerData: dict = {}
+    lock = mp.Lock()
+
+    def __init__(self, path_='.'):
+        self.path_ = path_
+
+    def run(self, file_path):
+        with open(file_path, newline='') as cf:
             data_ = csv.reader(cf)
             price_list = []
             for row in data_:
                 try:
                     price_list.append(float(row[2]))
-                except Exception as e:
-                    # log.error(e.args)
+                except:
                     pass
             tick_name = row[0]
             price_list.sort()
@@ -53,55 +92,9 @@ class TickerReader(Process):
             average_price = (price_list[0] + price_list[-1]) / 2
             volatility = ((price_list[-1] - price_list[0]) / average_price) * 100
             # print(f'{tick_name} волатильность {volatility}')
-            print(id(self), id(TickerReader), id(Tickers.TickerData))  # TODO id все время разные!!!
-            Tickers.TickerData[tick_name] = volatility
-            print(Tickers.TickerData)
-
-
-class Tickers():
-    TickerData: dict = {}
-
-    def __init__(self, path_='.'):
-        self.path_ = path_
-
-    def print_rez(self, data):
-        """
-        Печать результата в формате:
-            Максимальная волатильность:
-            ТИКЕР1 - ХХХ.ХХ %
-            ТИКЕР2 - ХХХ.ХХ %
-            ТИКЕР3 - ХХХ.ХХ %
-        Минимальная волатильность:
-            ТИКЕР4 - ХХХ.ХХ %
-            ТИКЕР5 - ХХХ.ХХ %
-            ТИКЕР6 - ХХХ.ХХ %
-        Нулевая волатильность:
-            ТИКЕР7, ТИКЕР8, ТИКЕР9, ТИКЕР10, ТИКЕР11, ТИКЕР12
-        Волатильности указывать в порядке убывания. Тикеры с нулевой волатильностью упорядочить по имени.
-        :param data: Словарь с данными для вывода
-        :return: None
-        """
-        log = logging.getLogger(__name__)
-        # ticker_list = list(map(lambda x, y: [y, x], data.keys(), data.values()))
-        ticker_list = list(zip(data.values(), data.keys()))
-        ticker_list.sort()
-        zero_list = []
-        while ticker_list and ticker_list[0][0] == 0:
-            zero_list.append(ticker_list[0][1])
-            ticker_list.pop(0)
-        zero_list.sort()
-        try:
-            print('\nМаксимальная волатильность:')
-            for i in range(3):
-                print(
-                    f'\t{ticker_list[len(ticker_list) - i - 1][1]} - {ticker_list[len(ticker_list) - i - 1][0]:0.2f} %')
-            print('\nМинимальная волатильность:')
-            for i in range(3):
-                print(f'\t{ticker_list[2 - i][1]} - {ticker_list[2 - i][0]:0.2f} %')
-        except Exception as e:
-            log.error(e.args)
-        print('\nНулевая волатильность:')
-        print(', '.join(zero_list))
+            # print(id(self), id(TickerReader.TickerData))
+            with TickerReader.lock:
+                TickerReader.TickerData[tick_name] = volatility
 
     def main(self):
         """
@@ -115,17 +108,17 @@ class Tickers():
             for fl_ in files:
                 filepath = path.join(dir_, fl_)
                 print(f'Обнаружен файл: {fl_}, обрабатываем...')
-                ps = TickerReader(filepath)
+                ps = mp.Process(target=self.run(filepath))
                 ps.start()
                 processes.append(ps)
         for tr in processes:
             tr.join()
-        self.print_rez(Tickers.TickerData)
+        print_rez(TickerReader.TickerData)
 
 
 if __name__ == '__main__':
     time_start = time()
-    ticker = Tickers('./trades/')
+    ticker = TickerReader('./trades/')
     ticker.main()
     time_end = time()
     print(f'\nОбработка производилась: {time_end - time_start} сек.')
